64eb804b25d492ce6f9a38a2dc00ed4e5b8c8383
ehyeon0112 <ehyeon0112@naver.com>
2026-09-01 17:27:24 +0900

    feat: add stage map collision areas

--- client/src/scenes/StageSelectScene.ts
+++ client/src/scenes/StageSelectScene.ts
@@ -26,10 +26,115 @@ export class StageSelectScene extends Phaser.Scene {
         width / 2,
         height / 2,
         'player',
-        0.05
+        0.045
       );
 
-    };
+    //왼쪽 위 큰 집
+    const house1Collider = this.add.rectangle(
+        390,      // x 좌표
+        195,      // y 좌표
+        380,      // 가로 길이
+        170,      // 세로 길이
+        0xff0000, // 빨간색
+        0.3       // 투명도
+      );
+
+      this.physics.add.existing(house1Collider, true);
+      this.physics.add.collider(
+        this.player,
+        house1Collider
+      );
+
+    // 가운데 위쪽 작은 집 1
+    const house2Collider = this.add.rectangle(
+        810,
+        175,
+        120,
+        130,
+        0xff0000,
+        0.3
+    );
+    
+    this.physics.add.existing(house2Collider, true);
+    this.physics.add.collider(this.player, house2Collider);
+    
+    
+    // 가운데 위쪽 작은 집 2
+    const house3Collider = this.add.rectangle(
+        1035,
+        175,
+        120,
+        130,
+        0xff0000,
+        0.3
+    );
+    
+    this.physics.add.existing(house3Collider, true);
+    this.physics.add.collider(this.player, house3Collider);
+    
+    
+    // 오른쪽 위 큰 집
+    const house4Collider = this.add.rectangle(
+        1570,
+        210,
+        420,
+        220,
+        0xff0000,
+        0.3
+    );
+    
+    this.physics.add.existing(house4Collider, true);
+    this.physics.add.collider(this.player, house4Collider);
+    
+    
+    // 아래쪽 큰 건물
+    const house5Collider = this.add.rectangle(
+        780,
+        850,
+        750,
+        330,
+        0xff0000,
+        0.3
+    );
+    
+    this.physics.add.existing(house5Collider, true);
+    this.physics.add.collider(this.player, house5Collider);
+    
+
+    // 지붕 
+    const roof1 = this.add.rectangle(
+        1580,
+        70,
+        80,
+        25,
+        0xff0000,
+        0.3
+      );
+      
+      const roof2 = this.add.rectangle(
+        1580,
+        95,
+        140,
+        25,
+        0xff0000,
+        0.3
+      );
+      
+
+
+      this.physics.add.existing(roof1, true);
+      this.physics.add.existing(roof2, true);
+ 
+      
+      this.physics.add.collider(this.player, roof1);
+      this.physics.add.collider(this.player, roof2);
+
+
+
+
+    }
+
+    
 
     update(): void {
         this.player.update();
